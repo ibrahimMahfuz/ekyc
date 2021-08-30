913c9d5985deca95791fe8cdac4b0c088f152cc0
Your Name <you@example.com>
2021-08-30 20:07:50 +0700

    cors

--- src/main/java/id/co/pcsindonesia/ia/ekyc/controller/EkycController.java
+++ src/main/java/id/co/pcsindonesia/ia/ekyc/controller/EkycController.java
@@ -22,10 +22,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -33,6 +30,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 @AllArgsConstructor
