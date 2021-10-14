1045d7d2431e9843b4c778a4eba72d4d9d948ed0
Your Name <you@example.com>
2021-10-14 12:56:04 +0700

    fix bugs

--- src/main/java/id/co/pcsindonesia/ia/ekyc/dto/query/vida/VidaExtraDto.java
+++ src/main/java/id/co/pcsindonesia/ia/ekyc/dto/query/vida/VidaExtraDto.java
@@ -2,17 +2,13 @@ package id.co.pcsindonesia.ia.ekyc.dto.query.vida;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class VidaExtraDto {
     private List<VidaExtraFieldDto> fields;
