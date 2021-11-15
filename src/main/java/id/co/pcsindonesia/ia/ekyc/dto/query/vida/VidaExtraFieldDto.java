712f82cfc1ab34415189fd8ab4a88f4ac2b20986
Your Name <you@example.com>
2021-11-15 16:23:41 +0700

    enhance feature: composition (ocr + demogs)

--- src/main/java/id/co/pcsindonesia/ia/ekyc/dto/query/vida/VidaExtraFieldDto.java
+++ src/main/java/id/co/pcsindonesia/ia/ekyc/dto/query/vida/VidaExtraFieldDto.java
@@ -2,15 +2,11 @@ package id.co.pcsindonesia.ia.ekyc.dto.query.vida;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class VidaExtraFieldDto {
     private String field;
